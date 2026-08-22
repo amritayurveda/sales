b938312e4ba4952b28334534a07008a0b573ad08
AYURVEDA CRM <wattjames428@gmail.com>
2026-08-22 17:11:05 +0530

    Fix Vercel serverless function export and catch-all routing

--- api/index.js
+++ api/index.js
@@ -1,17 +1,4 @@
 // api/index.js - Vercel Serverless Function Entrypoint
 const app = require('../server');
-const { initDb } = require('../config/db');
 
-let lastInitTime = 0;
-
-module.exports = async (req, res) => {
-  if (Date.now() - lastInitTime > 4000) {
-    try {
-      await initDb();
-      lastInitTime = Date.now();
-    } catch (e) {
-      console.error('Vercel initDb warning:', e.message);
-    }
-  }
-  return app(req, res);
-};
+module.exports = app;
